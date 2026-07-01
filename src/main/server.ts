1caec998cc4caccc3fd8ae0e84a462ccec5d2358
Kyyn <85359416+TheKyyn@users.noreply.github.com>
2026-07-01 13:52:17 +0200

    load .env at server startup

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
+// Charge les variables de .env dans process.env AVANT toute lecture de config.
+// Confiné au point d'entrée : le domaine et l'application n'en dépendent pas.
+import 'dotenv/config'
+
 import { buildContainer, toAppDependencies } from './container'
 import { createApp } from '../interface/http/app'
 import { loadEnv } from '../infrastructure/config/env'
